40bacdfdc25ab320ffb5ec5438a9e3868f44a685
allemkarthik <allemkarthik@gmail.com>
2026-02-12 18:52:38 -0800

    gpt suggestion movies fetched

--- src/pages/GptMovieSuggestion.jsx
+++ src/pages/GptMovieSuggestion.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import MovieList from "../components/movieList";
 
 const GptMovieSuggestion = () => {
+  const gpt = useSelector((store) => store.gpt);
+  const { movieResults, movieNames } = gpt;
+  if (movieNames == null) return null;
   return (
-    <div>
-      <h1>movie suggestions</h1>
+    <div className="my-6 mx-2 bg-black">
+      <div className="">
+        {movieNames.map((movie,index) => (
+          <MovieList key={movie} title={movie} movies={movieResults[index]} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GptMovieSuggestion
+export default GptMovieSuggestion;
